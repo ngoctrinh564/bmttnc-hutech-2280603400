073905d56d244be2c237301525e52fa20cfda71b
Trinh Nguyen Ngoc <nguyenngoctrinhbt3@gmail.com>
2025-05-26 23:57:45 +0700

    done

--- LAB_02/api.py
+++ LAB_02/api.py
@@ -6,25 +6,23 @@ from cipher.playfair import PlayFairCipher
 
 app = Flask(__name__)
 
-# Khởi tạo đối tượng mã hóa
+#CAESAR CIPHER ALGORITHM
 caesar_cipher = CaesarCipher()
-vigenere_cipher = VigenereCipher()
 
-# ===== Caesar Cipher APIs =====
-@app.route('/api/caesar/encrypt', methods=['POST'])
+@app.route("/api/caesar/encrypt", methods=["POST"])
 def caesar_encrypt():
     data = request.json
-    plain_text = data.get('plain_text', '')
-    key = int(data.get('key', 0))
-    encrypted_text = caesar_cipher.encrypt(plain_text, key)
+    plain_text = data['plain_text']
+    key = int(data['key'])
+    encrypted_text = caesar_cipher.encrypt_text(plain_text, key)
     return jsonify({'encrypted_message': encrypted_text})
 
-@app.route('/api/caesar/decrypt', methods=['POST'])
+@app.route("/api/caesar/decrypt", methods=["POST"])
 def caesar_decrypt():
     data = request.json
-    cipher_text = data.get('cipher_text', '')
-    key = int(data.get('key', 0))
-    decrypted_text = caesar_cipher.decrypt(cipher_text, key)
+    cipher_text = data['cipher_text']
+    key = int(data['key'])
+    decrypted_text = caesar_cipher.decrypt_text(cipher_text, key)
     return jsonify({'decrypted_message': decrypted_text})
 
 #VIGENERE CIPHER ALGORITHM
